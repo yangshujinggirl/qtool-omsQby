c6d74ce67130c0a41ab22fceca9b5f8ae75e51ab
LorenWang <wl@qtoolsbaby.com>
2020-03-09 19:08:37 +0800

    新增门店退单列表页面（无接口，未测试）
    页面中点击按钮使用Modal.confirm，没有使用变量控制显示与否

---
+++ src/pages/OrderCenter/Border/ShopReturnOrder/components/FilterForm/index.js
@@ -0,0 +1,83 @@
+import React from "react";
+import {Form, Input, Select, Row, Col, DatePicker} from "antd";
+import {Qbtn} from "common";
+import moment from "moment";
+import {BaseFilter} from "common/index";
+import {AUDIT_STATUS_SENT, AUDIT_STATUS_WAIT_SEND} from "../../config";
+
+const FormItem = Form.Item;
+const {Option} = Select;
+const {RangePicker} = DatePicker;
+
+export default class SearchForm extends BaseFilter {
+    formRef = React.createRef();
+
+    /**
+     * 第一次渲染之后调用数据
+     */
+    componentDidMount = () => {
+        this.formRef.current.setFieldsValue({
+            orderTimes: [moment(this.props.stime), moment(this.props.etime)]
+        })
+    };
+
+    render() {
+        return (
+            <div className="qtoolOms-condition">
+                <Form className="serach-common-form" ref={this.formRef}{...this.formItemLayout}>
+                    <Row gutter={24}>
+                        <Col {...this.colspans}>
+                            <FormItem label="退货单号" name="stockingCode" {...this.formItemLayout}>
+                                <Input placeholder="请输入退货单号" autoComplete="off"/>
+                            </FormItem>
+                        </Col>
+                        <Col {...this.colspans}>
+                            <FormItem name="suppliersName" label="门店名称" {...this.formItemLayout}>
+                                <Input placeholder="请输入门店名称" autoComplete="off"/>
+                            </FormItem>
+                        </Col>
+                        <Col {...this.colspans}>
+                            <FormItem name="itemCode" label="关联门店订单号" {...this.formItemLayout}>
+                                <Input placeholder="请输入关联门店订单号" autoComplete="off"/>
+                            </FormItem>
+                        </Col>
+                        <Col {...this.colspans}>
+                            <FormItem label="商品编码" name="itemName" {...this.formItemLayout}>
+                                <Input placeholder="请输入商品编码" autoComplete="off"/>
+                            </FormItem>
+                        </Col>
+                        <Col {...this.colspans}>
+                            <FormItem label="商品名称" name="itemName" {...this.formItemLayout}>
+                                <Input placeholder="请输入商品名称" autoComplete="off"/>
+                            </FormItem>
+                        </Col>
+                        <Col {...this.colspans}>
+                            <FormItem label="订单状态" name="step" {...this.formItemLayout}>
+                                <Select placeholder="请选择订单状态" allowClear={true}>
+                                    <Option value={AUDIT_STATUS_WAIT_SEND}>待发货</Option>
+                                    <Option value={AUDIT_STATUS_SENT}>已发货</Option>
+                                </Select>
+                            </FormItem>
+                        </Col>
+                        <Col {...this.colspans}>
+                            <FormItem label="下单时间" name="orderTimes" rul {...this.formItemLayout}>
+                                <RangePicker
+                                    placeholder={this.placeholder}
+                                    format={this.formatType}
+                                    showTime
+                                />
+                            </FormItem>
+                        </Col>
+                    </Row>
+                </Form>
+                <Col offset={21}>
+                    <FormItem className="oms-condition-operate">
+                        <Qbtn type="primary" onClick={this.handleSubmit}>
+                            搜索
+                        </Qbtn>
+                    </FormItem>
+                </Col>
+            </div>
+        );
+    }
+}
--- src/pages/OrderCenter/Border/ShopReturnOrder/index.js
+++ src/pages/OrderCenter/Border/ShopReturnOrder/index.js
@@ -1,13 +1,166 @@
-import React, { Component } from 'react';
+import React from 'react';
+import {BaseDataShowList} from "common/QbaseDataShowList";
+import FilterForm from "./components/FilterForm";
+import Columns from "./column";
+import {GetShopOrderListApi} from "../../../../api/home/OrderCenter/Border/ShopOrder";
+import {Link} from "react-router-dom";
+import {Qbtn, Qmessage} from "common/index";
+import {
+    EXPORT_TYPE_PURCHASE_ORDER_IN,
+    EXPORT_TYPE_PURCHASE_ORDER_OUT,
+    ExportApi,
+    getExportData
+} from "../../../../api/Export";
+import {PushPurchaseInOrderForceComplete} from "../../../../api/home/OrderCenter/PurchaseOrder/PurchaseIn";
+import {Modal} from "antd";
 
-class index extends Component {
-    render() {
-        return (
-            <div>
-                11
-            </div>
-        );
+/**
+ * 功能作用：门店退单列表页面
+ * 初始注释时间： 2020/3/9 18:42
+ * 注释创建人：LorenWang（王亮）
+ * 方法介绍：
+ * 思路：
+ * 修改人：
+ * 修改时间：
+ * 备注：
+ */
+export default class ShopReturnOrder extends BaseDataShowList {
+    /**
+     * 确认收货key
+     * @type {number}
+     */
+    tipsTextKeyConfirmReceipt = 1;
+    /**
+     * 确认退款key
+     * @type {number}
+     */
+    tipsTextKeyConfirmTheRefund = 2;
+    /**
+     * 取消订单key
+     * @type {number}
+     */
+    tipsTextKeyCancellationOfOrder = 3;
+
+    /**
+     * 初始化
+     */
+    constructor() {
+        super();
+        //设置列表操作key字段
+        this.dataListOptionsKey = "stockingCode";
+        //显示行操作
+        this.isShowTableRowSelection = true;
+        //设置表字段
+        this.tableShowColumns = Columns
     }
-}
 
-export default index;
+    /**
+     * 显示弹窗
+     * @param key 弹窗显示的key值
+     */
+    showModalClick = (key) => {
+        //判断是否有选择
+        if (this.state.selectedRowKeys.length === 0) {
+            Qmessage.warn("请至少选择一个采购单")
+        } else {
+            switch (key) {
+                case this.tipsTextKeyConfirmReceipt:
+                    Modal.confirm({
+                        title: "确认收货",
+                        content: "确认收货后，所选退单将不支持继续收货，是否确定执行？",
+                        okText: "确定",
+                        cancelText: "取消",
+                        onOk: () => {
+                            Qmessage.success("点击了确认收货确认");
+                        }
+                    });
+                    break;
+                case this.tipsTextKeyConfirmTheRefund:
+                    Modal.confirm({
+                        title: "确认退款",
+                        content: "确认退款后，将会退款到门店掌柜余额中，是否确定执行？",
+                        okText: "确定",
+                        cancelText: "取消",
+                        onOk: () => {
+                            Qmessage.success("点击了确认退款确认");
+                        }
+                    });
+                    break;
+                case this.tipsTextKeyCancellationOfOrder:
+                    Modal.confirm({
+                        title: "取消订单",
+                        content: "取消订单后，所选退单将被取消，是否确定执行？",
+                        okText: "确定",
+                        cancelText: "取消",
+                        onOk: () => {
+                            Qmessage.success("点击了取消订单确认");
+                        }
+                    });
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
+    /**
+     * 格式化搜索条件
+     * @param values
+     * @return {{}}
+     */
+    formatSearchCriteriaList(values) {
+        let {searchCriteriaDefaultStartTime, searchCriteriaDefaultEndTime} = this.state;
+        if (values != null && values.orderTimes != null) {
+            searchCriteriaDefaultStartTime = values.orderTimes[0].format(this.timeFormatYMDStr);
+            searchCriteriaDefaultEndTime = values.orderTimes[1].format(this.timeFormatYMDStr);
+            values.times = null
+        }
+        this.setState({
+            searchCriteriaDefaultStartTime, searchCriteriaDefaultEndTime,
+            searchCriteriaList: {
+                stime: searchCriteriaDefaultStartTime,
+                etime: searchCriteriaDefaultEndTime
+            }
+        });
+        return this.state.searchCriteriaList;
+    }
+
+    /**
+     * 获取数据列表请求
+     * @param params 参数
+     * @return {*}
+     */
+    getDataListRequest(params) {
+        return GetShopOrderListApi(params)
+    }
+
+    /**
+     * 获取搜索筛选渲染部分
+     * @return {*}
+     */
+    getRenderFilterForm() {
+        return <FilterForm onSubmit={this.searchDataList}
+                           stime={this.state.searchCriteriaDefaultStartTime}
+                           etime={this.state.searchCriteriaDefaultEndTime}/>
+    }
+
+    /**
+     * 获取操作按钮列表
+     * @param defaultContainerClsName 默认容器样式类名
+     */
+    getRenderOperateBtnAction(defaultContainerClsName) {
+        return <div className={defaultContainerClsName}>
+            <Link to='/account/add_purchasein'><Qbtn size="free">新建退单</Qbtn></Link>
+            <Qbtn size="free"
+                  onClick={() => this.showModalClick(this.tipsTextKeyConfirmReceipt)}>确认收货</Qbtn>
+            <Qbtn size="free"
+                  onClick={() => this.showModalClick(this.tipsTextKeyConfirmTheRefund)}>确认退款</Qbtn>
+            <Qbtn size="free"
+                  onClick={() => this.showModalClick(this.tipsTextKeyCancellationOfOrder)}>取消订单</Qbtn>
+            <Qbtn size="free"
+                  onClick={() => ExportApi(getExportData(this.state.searchCriteriaList.stime, this.state.searchCriteriaList.etime,
+                      EXPORT_TYPE_PURCHASE_ORDER_OUT, this.state.searchCriteriaList))}>导出数据</Qbtn>
+        </div>
+    }
+
+}
