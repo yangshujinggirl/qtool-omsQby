849cd67299c201870327da1f530f2730594b6dba
LorenWang <wl@qtoolsbaby.com>
2020-03-10 10:47:38 +0800

    解决冲突

--- src/pages/OrderCenter/Border/ShopOrder/column/index.js
+++ src/pages/OrderCenter/Border/ShopOrder/column/index.js
@@ -1,3 +1,4 @@
+import { Form,Input } from 'antd';
 import moment from "moment";
 import {Link} from "react-router-dom";
 import React from "react";
@@ -40,4 +41,48 @@ const Columns = [
         render: text => <span>{text && moment(text).format("YYYY-MM-DD HH:mm:ss")}</span>
     }
 ];
-export default Columns;
+const columnsAdd=(onBlur)=>{
+  return [{
+            title: "sku编码",
+            dataIndex: "skuCode",
+            textWrap: 'word-break',
+            render:(text,record,index)=> {
+              return  <Form.Item name={['list',index,'barCode']}>
+                        <Input
+                          className="goods-name"
+                          key={index}
+                          onBlur={(e)=>onBlur(e,index)}/>
+                      </Form.Item>
+            }
+          },{
+            title: "订购数量",
+            dataIndex: "salesAttributeName",
+            textWrap: 'word-break',
+            render:(text,record,index)=> {
+              return  <Form.Item name={['list',index,'barCode']}>
+                        <Input disabled={record.isExamine} className="goods-name" key={index}/>
+                      </Form.Item>
+            }
+          },{
+            title: "商品名称",
+            dataIndex: "salesAttributeName",
+            textWrap: 'word-break',
+          },{
+            title: "商品规格",
+            dataIndex: "salesAttributeName",
+            textWrap: 'word-break',
+          },{
+            title: "B端售价",
+            dataIndex: "outerProductCode",
+            textWrap: 'word-break',
+          },{
+            title: "金额小计",
+            textWrap: 'word-break',
+            dataIndex: "dhPrice",
+          }]
+};
+
+
+export {
+   columnsAdd,Columns
+}
