e044ec96187a9cd2eb6383d95f35b6147c2fae99
LorenWang <wangliang_worljob@163.com>
2020-03-15 18:37:12 +0800

    新增card相关组件，主要用于基础数据部分迁移

---
+++ src/common/QcardList/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {Card} from 'antd';
+import {CaretDownOutlined, CaretUpOutlined} from '@ant-design/icons';
+import './index.css'
+import {Link} from "react-router-dom";
+
+export default class QcardList extends React.Component {
+    render() {
+        return (
+            <div className='q-card-list'>
+                {
+                    this.props.data.map((item, index) => {
+                        return (
+                            <Link style={{
+                                width: 220,
+                                color: "#fff",
+                                padding: '10px 0',
+                                cursor: 'pointer',
+                                background: item.bg
+                            }} key={index} to={item.linkToPage}>
+                                <p>{item.title}</p>
+                                <p className='q-card-list-value'>{item.value}</p>
+                            </Link>
+                        )
+                    })
+                }
+
+            </div>
+        );
+    }
+}
