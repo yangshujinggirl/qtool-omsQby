f92f349e94c5fe9c87401a72062de32a2c651b8e
LorenWang <wangliang_worljob@163.com>
2020-03-15 20:55:24 +0800

    新增商品数据商品数据列表页面
    使用假数据，同时做了数据转换，方便数据操作

--- src/pages/DataCenter/BaseData/GoodsData/GoodsDataList/index.js
+++ src/pages/DataCenter/BaseData/GoodsData/GoodsDataList/index.js
@@ -1,4 +1,13 @@
 import React from 'react'
+import {QbaseList, Qbtn, Qpagination, Qtable} from "common/index";
+import FilterForm from "./components/FilterForm";
+import {
+    ErpExportApi,
+    EXPORT_TYPE_PURCHASE_ORDER_OUT,
+    getExportData
+} from "../../../../../api/Export";
+import Columns from "./column";
+import {GetGoodsDataList} from "../../../../../api/home/DataCenter/BaseData/GoodsData";
 
 /**
  * 功能作用：商品分析
@@ -10,8 +19,28 @@ import React from 'react'
  * 修改时间：
  * 备注：
  */
-export default class GoodsDataList extends React.Component {
-    render() {
-        return <div>商品数据</div>
-    }
-}
+const GoodsDataList = QbaseList((_this) => {
+        const {
+            dataList, everyPage, currentPage, totalCount
+        } = _this.state;
+        return (
+            <div className="oms-common-index-pages-wrap">
+                <FilterForm onSubmit={_this.searchDataList} selectTimeChange={_this.selectTimeChange}/>
+                <div className="handle-operate-btn-action">
+                    <Qbtn size="free"
+                          onClick={() => new ErpExportApi(getExportData(this.state.searchCriteriaList.stime, this.state.searchCriteriaList.etime,
+                              EXPORT_TYPE_PURCHASE_ORDER_OUT, this.state.searchCriteriaList))}>导出数据</Qbtn>
+                </div>
+                <Qtable
+                    columns={Columns}
+                    select={true}
+                    dataSource={dataList}/>
+                <Qpagination
+                    data={{everyPage, currentPage, totalCount}}
+                    onChange={_this.changePage}/>
+            </div>
+        );
+    }, GetGoodsDataList,
+    false, "stockingCode", null,
+    null, null);
+export default GoodsDataList;
