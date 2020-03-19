dde6c8d38c84d35e101b2994e90fe6f57db5188f
LorenWang <wl@qtoolsbaby.com>
2020-03-19 11:29:39 +0800

    新增财务中心-掌柜收支明细页面

--- src/api/home/FinancialCenter/ShoperInOut.js
+++ src/api/home/FinancialCenter/ShoperInOut.js
@@ -1,17 +1,12 @@
-import { erpAjax } from "../../../Req";
-//列表
+import {erpAjax} from "./../../Req";
+
+/**
+ * 掌柜收支明细列表
+ * @param values
+ * @return {*}
+ */
 export function getListApi(values) {
-  return erpAjax.get("/banner/queryList", {
-    params: values
-  });
-}
-//详情
-export function getInfosApi(id) {
-  return erpAjax.get(`/banner/getById/${id}`);
-}
-//保存
-export function saveApi(values) {
-  return erpAjax.post("/banner/save", {
-    ...values
-  });
+    return erpAjax.get("/spmoney/detail/query", {
+        params: values
+    });
 }
