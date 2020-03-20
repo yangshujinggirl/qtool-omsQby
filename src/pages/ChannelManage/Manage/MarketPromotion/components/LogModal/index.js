895a42f9d7532f9916fa2d98d917945e4aea2b77
LorenWang <wl@qtoolsbaby.com>
2020-03-20 10:56:23 +0800

    新增渠道管理-市场推广列表页面以及详情页面
    接口简单对接，同时当前所有接口已通，但未进行细节测试

---
+++ src/pages/ChannelManage/Manage/MarketPromotion/components/LogModal/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {QbaseList, Qpagination, Qtable} from "common/index";
+import LogColumns from "../../column/LogModal";
+import './index.css'
+import {GetMarketPromotionLogList} from "../../../../../../api/home/ChannelManage/Manager/MarketPromotion";
+
+/**
+ * 弹窗日志显示
+ */
+const LogModal = QbaseList((logThis) => {
+    const {
+        dataList, everyPage, currentPage, totalCount
+    } = logThis.state;
+    return (
+        <div className="oms-common-index-pages-wrap cm-market-promotion-log-modal">
+            <Qtable
+                columns={LogColumns}
+                select={true}
+                dataSource={dataList}/>
+            <Qpagination
+                data={{everyPage, currentPage, totalCount}}
+                onChange={logThis.changePage}/>
+        </div>
+    )
+}, (params, _this) => new GetMarketPromotionLogList(_this.props.requestId), true);
+export default LogModal;
