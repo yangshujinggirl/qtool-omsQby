ececf96ded0685f8bd6eb1531f85c0dee5ccc4e4
zhy <17701799531@163.com>
2020-04-07 15:53:13 +0800

    删除react-dnd相关组件

--- src/common/QcontrolContext/index.js
+++
@@ -1,6 +0,0 @@
-import HTML5Backend from 'react-dnd-html5-backend';
-import { DragDropContext } from 'react-dnd';
-console.log(DragDropContext)
-
-// export default DragDropContext(HTML5Backend);
-export default DragDropContext;
