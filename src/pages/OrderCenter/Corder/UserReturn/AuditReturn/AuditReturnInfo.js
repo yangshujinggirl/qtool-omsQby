671a3d093e8cb01f4ad4cba3a7714bca7f65ffd2
zhouhongye <zhy@zhouhongyedeMacBook-Pro.local>
2020-04-28 21:05:24 +0800

    审核

--- src/pages/OrderCenter/Corder/UserReturn/AuditReturn/AuditReturnInfo.js
+++ src/pages/OrderCenter/Corder/UserReturn/AuditReturn/AuditReturnInfo.js
@@ -9,7 +9,6 @@ const TextArea = Input.TextArea;
 /**
  * 审核  zhy
  */
-<<<<<<< HEAD
 const AuditReturnInfo = (props) => {
 	const [form] = Form.useForm();
 	const [status, setStatus] = useState('');
@@ -30,33 +29,11 @@ const AuditReturnInfo = (props) => {
 				}
 			}
 		});
-=======
-const AuditReturnInfo = props => {
-  const [form] = Form.useForm();
-  const [status, setStatus] = useState("");
-  const [infos, setInfos] = useState({});
-  const [detailList, setDetailList] = useState("");
-  const { id } = props.match.params;
-  useEffect(() => {
-    getInfoApi({ reOrderNo: id }).then(res => {
-      if (res.httpCode == 200) {
-        const {detailList,...infos} = res.result;
-        setInfos(infos);
-        if(detailList&&detailList.length>0){
-          detailList.map((item,index)=>{
-            item.key = index
-          })
-          setDetailList(detailList);
-        }
-      }
-    });
->>>>>>> 6a8f4968f1c603266609aef7215025c66f75ed0a
   }, []);
   //审核状态修改
 	const onChange = (e) => {
 		setStatus(e.target.value);
   };
-<<<<<<< HEAD
   //退款方式更改
 	const returnTypeChange = (e) => {
 		setIsInvented(e.target.value);
@@ -163,109 +140,5 @@ const AuditReturnInfo = props => {
 			</Card>
 		</div>
 	);
-=======
-  /**
-   * 提交
-   */
-  const handleSubmit = async () => {
-    const values = await form.validateFields();
-    values.operation = 1;
-    values.reOrderNo=id;
-    operateReturnApi(values).then(res => {
-      if (res.httpCode == 200) {
-        props.history.push("/account/subscriber_refund_orders");
-      }
-    });
-  };
-  const formLayout = {
-    labelCol:{span:2},
-    wrapperCol:{span:8}
-  }
-  return (
-    <div>
-      <Card title="退单信息" className="base_info">
-        <Form.Item label="退单号">{infos.reOrderNo}</Form.Item>
-        <Form.Item label="关联用户订单">{infos.channelOrderNo}</Form.Item>
-        <Form.Item label="订单类型">{infos.deliveryTypeStr}</Form.Item>
-        <Form.Item label="用户手机号">{infos.phone}</Form.Item>
-        {/*<Form.Item label="用户昵称">{infos.subject}</Form.Item>*/}
-        <Form.Item label="退款类型">{infos.refundTypeStr}</Form.Item>
-        <Form.Item label="退款运费">{infos.rePostage}</Form.Item>
-        <Form.Item label="退款商品金额">{infos.totalPrice}</Form.Item>
-        <Form.Item label="退款总金额">{infos.totalPrice}</Form.Item>
-        <Form.Item label="创建时间">
-          {infos.createTime &&
-            moment(infos.createTime).format("YYYY-MM-DD HH:mm:ss")}
-        </Form.Item>
-      </Card>
-      <Card title="订单商品">
-        <Qtable columns={AbnormalGoodsColumns} dataSource={detailList} />
-      </Card>
-      <Card title="异常处理">
-        <Form form={form} {...formLayout}>
-          <Form.Item
-            name="status"
-            label="审核结果"
-            rules={[{ required: true, message: "请选择审核结果" }]}
-          >
-            <Radio.Group onChange={onChange}>
-              <Radio value={20}>同意退款</Radio>
-              <Radio value={99}>拒绝退款</Radio>
-            </Radio.Group>
-          </Form.Item>
-          {status == 20 && (
-            <React.Fragment>
-              <Form.Item
-                name="isInvented"
-                label="退款类型"
-                rules={[{ required: true, message: "请选择退款类型" }]}
-              >
-                <Radio.Group>
-                  <Radio value={false}>退货退款</Radio>
-                  <Radio value={true}>仅退款</Radio>
-                </Radio.Group>
-              </Form.Item>
-              <Form.Item
-                name="consignee"
-                label="收货人"
-                rules={[{ required: true, message: "请输入收货人" }]}
-              >
-                <Input placeholder="请输入收货人" autoComplete="off" />
-              </Form.Item>
-              <Form.Item
-                name="shPhone"
-                label="收货电话"
-                rules={[{ required: true, message: "请输入收货电话" }]}
-              >
-                <Input placeholder="请输入收货电话" autoComplete="off" />
-              </Form.Item>
-              <Form.Item
-                name="shAddress"
-                label="收货地址"
-                rules={[{ required: true, message: "请输入收货地址" }]}
-              >
-                <Input placeholder="请输入收货地址" autoComplete="off" />
-              </Form.Item>
-            </React.Fragment>
-          )}
-          {status == 99 && (
-            <Form.Item
-              name="refusalReasons"
-              label="拒绝原因"
-              rules={[{ required: true, message: "请输入拒绝原因" }]}
-            >
-              <TextArea placeholder="拒绝原因将展示给用户，请谨慎填写" />
-            </Form.Item>
-          )}
-          <Form.Item>
-            <Button type="primary" size="large" onClick={handleSubmit}>
-              提交
-            </Button>
-          </Form.Item>
-        </Form>
-      </Card>
-    </div>
-  );
->>>>>>> 6a8f4968f1c603266609aef7215025c66f75ed0a
 };
 export default AuditReturnInfo;
