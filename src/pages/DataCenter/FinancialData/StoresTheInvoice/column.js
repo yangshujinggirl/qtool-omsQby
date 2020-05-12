e332283a46d02f80f9ef4e1c6544c0bbcdd75039
zhouhongye <zhy@zhouhongyedeMacBook-Pro.local>
2020-05-12 19:19:06 +0800

    门店发票

---
+++ src/pages/DataCenter/FinancialData/StoresTheInvoice/column.js
@@ -0,0 +1,23 @@
+const Columns = [
+	{
+		title: '门店名称',
+		dataIndex:'shopName'
+    },
+    {
+		title: '销售总金额',
+		dataIndex:'saleAmount'
+    },
+    {
+		title: '销售数量',
+		dataIndex:'saleQty'
+    },
+    {
+		title: '退货总金额',
+		dataIndex:'returnAmount'
+    },
+    {
+		title: '退货数量',
+		dataIndex:'returnQty'
+    },
+];
+export default Columns;
--- src/pages/DataCenter/FinancialData/StoresTheInvoice/index.js
+++ src/pages/DataCenter/FinancialData/StoresTheInvoice/index.js
@@ -3,35 +3,59 @@ import { Qtable, Qpagination} from "common";
 import { GetStoresTheInvoiceList } from "api/home/DataCenter/FinancialData";
 import {Button} from 'antd'
 import FilterForm from "./components/FilterForm/index";
-// import Columns from "./columns";
+import moment from "moment";
+import Columns from './column'
 
 class StoresTheInvoice extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      everyPage: 0,
       currentPage: 0,
       everyPage: 0,
       dataList: [],
-      inputValues: {}
+      inputValues: {},
     };
   }
   componentDidMount() {
-    this.searchData({});
+    this.searchData({startDate:moment().format('YYYY-MM-DD')});
   }
   //点击搜索
   searchData = values => {
-    GetStoresTheInvoiceList(values).then(res => {
+    const {startDate,..._values} = values;
+    _values.startDate = moment(startDate).format('YYYY-MM-DD');
+    GetStoresTheInvoiceList(_values).then(res => {
       if (res.httpCode == 200) {
         const { result, everyPage, currentPage, total } = res.result;
-        if (result.length) {
-          result.map(item => (item.key = item.themeActivityId));
-        }
+        const tableTit = result[0].categoryAmountQtyDXOS;
+        tableTit.map(item=>{
+            const obj={
+                title:item.categoryName,
+                children: [
+                    {
+                        title: item.categoryName+'数量',
+                        dataIndex:item.categoryName+"saleQty",
+                    },
+                    {
+                        title: item.categoryName+'金额',
+                        dataIndex:item.categoryName+"saleAmount",
+                    },
+                ],
+            };
+            Columns.push(obj)
+        });
+        const dataList = result.map(item => {
+            item.categoryAmountQtyDXOS.map(ele=>{
+                item[ele.categoryName+'saleQty'] = ele.saleQty;
+                item[ele.categoryName+'saleAmount'] = ele.saleAmount;
+            })
+            return item
+        });
+        console.log(dataList)
         this.setState({
-          dataList: result,
+          dataList,
           everyPage,
           currentPage,
-          total
+          total,
         });
       }
     });
@@ -55,13 +79,13 @@ class StoresTheInvoice extends Component {
       <div className="oms-common-index-pages-wrap">
         <FilterForm onSubmit={this.searchData} />
         <div className="handle-operate-btn-action">
-            <Button>导出数据</Button>
+            <Button type='primary'>导出数据</Button>
         </div>
-        {/* <Qtable
-          dataSource={[]}
-          columns={[]}
+        <Qtable
+          dataSource={dataList}
+          columns={Columns}
           onOperateClick={this.handleOperateClick}
-        /> */}
+        />
         {dataList.length > 0 ? (
           <Qpagination
             data={{ everyPage, currentPage, total }}
